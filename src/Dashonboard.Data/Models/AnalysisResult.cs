16d83730773f1a9f05dbf1ca72abdce11aac7ff0
unknown <aforloney@gmail.com>
2021-01-03 21:39:20 -0500

    included ParseDiff package & got file chunks working, checking to see how regex parsing handles in commit messages to help drive project

--- src/Dashonboard.Data/Models/AnalysisResult.cs
+++ src/Dashonboard.Data/Models/AnalysisResult.cs
@@ -2,5 +2,20 @@
 {
     public class AnalysisResult
     {
+        public ActionType Action { get; set; }
+        public MetricType Metric { get; set; }
+        public string MetricName { get; set; }
+    }
+
+    public enum ActionType
+    {
+        Create,
+        Remove
+    }
+
+    public enum MetricType
+    {
+        Timing,
+        Datapoints
     }
 }
