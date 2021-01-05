9fe0f0b009441c8cde4660edcd54e774722025e8
unknown <aforloney@gmail.com>
2021-01-04 20:54:30 -0500

    added extension method for adds/deletes filtering

---
+++ src/Dashonboard.Services/Extenstions/LineDiffExtenstions.cs
@@ -0,0 +1,19 @@
+﻿using ParseDiff;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dashonboard.Services.Extenstions
+{
+    public static class LineDiffExtenstions
+    {
+        public static IEnumerable<LineDiff> GetAdds(this IEnumerable<LineDiff> lineDiffs)
+        {
+            return lineDiffs.Where(chg => chg.Add == true);
+        }
+
+        public static IEnumerable<LineDiff> GetDeletes(this IEnumerable<LineDiff> lineDiffs)
+        {
+            return lineDiffs.Where(chg => chg.Delete == true);
+        }
+    }
+}
